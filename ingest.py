8208cc07164b4c58a7380911f64ca0a70a4f7570
Arjun M S <64315213+arjun-ms@users.noreply.github.com>
2023-09-03 10:26:49 +0530

    Update ingest.py

--- ingest.py
+++ ingest.py
@@ -14,42 +14,17 @@ import os
 import time 
 from constants import CHROMA_SETTINGS
 
-persist_directory = "db"
+ 
+load_dotenv()
 
-# load_dotenv()
 
+def loadAndSplitDoc(temp_path):
+    print("inside load and split")
+    pdf_loader = PyPDFLoader(temp_path)
+    documents = pdf_loader.load()
 
-# def loadAndSplitDoc(temp_path):
-#     print("inside load and split")
-#     pdf_loader = PyPDFLoader(temp_path)
-#     documents = pdf_loader.load()
-
-    
-#     documents = text_splitter.split_documents(documents)
-
-    
-
-def main():
-    for root, dirs, files in os.walk("docs"):
-        for file in files:
-            if file.endswith(".pdf"):
-                print(file)
-                loader = PyPDFLoader(os.path.join(root, file))
-    documents = loader.load()
-    print("splitting into chunks")
-   text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    texts = text_splitter.split_documents(documents)
-    #create embeddings here
-    print("Loading sentence transformers model")
-    embeddings = SentenceTransformerEmbeddings(model_name="openai-gpt")
-    #create vector store here
-    print(f"Creating embeddings. May take some minutes...")
-    db = Chroma.from_documents(texts, embeddings, persist_directory=persist_directory, client_settings=CHROMA_SETTINGS)
-    db.persist()
-    db=None 
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    documents = text_splitter.split_documents(documents)
 
     print("text read and splitted")
     return documents
-
-if __name__ == "__main__":
-    main()
